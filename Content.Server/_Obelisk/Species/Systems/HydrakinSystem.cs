e534a4c81c49bc366bd8ea296e09731af3b77918
sneb <96957003+snebl@users.noreply.github.com>
2025-10-22 21:02:39 -0400

    heat buildup system

---
+++ Content.Server/_Obelisk/Species/Systems/HydrakinSystem.cs
@@ -0,0 +1,29 @@
+using Content.Server._Obelisk.Species.Systems.Components;
+using Content.Server.Temperature.Components;
+using Content.Shared.Mobs;
+using Content.Shared.Mobs.Components;
+
+namespace Content.Server._Obelisk.Species.Systems;
+
+public sealed class HydrakinSystem : EntitySystem
+{
+    public override void Update(float frameTime)
+    {
+        base.Update(frameTime);
+
+        var query = EntityQueryEnumerator<HydrakinHeatBuildupComponent, TemperatureComponent>();
+        while (query.MoveNext(out var uid, out var comp, out var temperature))
+        {
+            if (TryComp<MobStateComponent>(uid, out var mobState) &&
+                mobState.CurrentState != MobState.Alive)
+                return;
+
+            if (temperature.CurrentTemperature < comp.MinTemperature ||
+                temperature.CurrentTemperature > comp.MaxTemperature)
+                return;
+
+            temperature.CurrentTemperature += comp.Buildup * frameTime;
+        }
+    }
+
+}
